a1460d2fbbc046c4060d5167feb561e99a09577c
siddikur-dev <siddikur.dev@gmail.com>
2025-11-09 23:11:59 +0600

    create issue to send db

--- index.js
+++ index.js
@@ -22,8 +22,17 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const issuesCollection = client.db("issuesDB").collection("all-issues");
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    //insert issue data
+    app.post("/all-issues", async (req, res) => {
+      const issue = req.body;
+      const result = await issuesCollection.insertOne(issue);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -31,7 +40,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
